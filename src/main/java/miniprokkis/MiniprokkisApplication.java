8a760847c90437740238086d14f8e69dc32f20e3
Jaana <jaana.embrich-hakala@helsinki.fi>
2017-11-13 16:42:06 +0200

    Lisää pom.xml ja ensimmäiset tiedostot

---
+++ src/main/java/miniprokkis/MiniprokkisApplication.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miniprokkis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Jaana
+ */
+@SpringBootApplication
+public class MiniprokkisApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SandboxApplication.class, args);
+    }
+}
