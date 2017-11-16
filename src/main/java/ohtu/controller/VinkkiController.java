63685d87e7e6bb7e3b02cf0a284fdae958fb0fa9
Dennis A <28817715+Dforssi@users.noreply.github.com>
2017-11-17 00:10:04 +0200

    Controlleri, JpaRepository ja Vinkkiluokka - uusi yritys

---
+++ src/main/java/ohtu/controller/VinkkiController.java
@@ -0,0 +1,26 @@
+package ohtu.controller;
+
+import ohtu.domain.VinkkiRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class VinkkiController {
+
+    @Autowired
+    private VinkkiRepository vinkkiRepository;
+
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "<a href='/other'>linkki</a>";
+    }
+
+    @GetMapping("/other")
+    @ResponseBody
+    public String other() {
+        return "Hei Maailma!";
+    }
+}
---
+++ src/main/java/ohtu/domain/Vinkki.java
@@ -0,0 +1,10 @@
+package ohtu.domain;
+
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Vinkki extends AbstractPersistable<Long>  {
+
+}
