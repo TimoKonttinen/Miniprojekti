4d28cd01da810711ef88e571dd4839acb4832563
laimikko1 <mikko.s.laitinen@helsinki.fi>
2017-11-16 15:33:26 +0200

    DbUrl tulostus

--- src/main/java/ohtu/Controllers.java
+++ src/main/java/ohtu/Controllers.java
@@ -4,13 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @Controller
 public class Controllers {
 
     @GetMapping("/")
     @ResponseBody
     public String home() {
-        return "<a href='/other'>linkki</a>";
+            String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        return dbUrl;
     }
     
     @GetMapping("/other")
