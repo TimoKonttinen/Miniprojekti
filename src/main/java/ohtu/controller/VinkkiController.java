28e92face0c5276c448078b01a9c0f711ffbe731
Jaana <jaana.embrich-hakala@helsinki.fi>
2017-11-17 21:01:50 +0200

    Lisätty alustava sql-schema tiedosto Vinkille ja muokattu index.html ja kirjoittajat.html ja lisätty Controlleriin metodeita

--- src/main/java/ohtu/controller/VinkkiController.java
+++ src/main/java/ohtu/controller/VinkkiController.java
@@ -1,10 +1,15 @@
 package ohtu.controller;
 
+import ohtu.domain.Vinkki;
 import ohtu.domain.VinkkiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class VinkkiController {
@@ -13,13 +18,22 @@ public class VinkkiController {
     private VinkkiRepository vinkkiRepository;
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("vinkit", vinkkiRepository.findAll());
         return "index";
     }
 
-    @GetMapping("/other")
-    @ResponseBody
-    public String other() {
-        return "Hei Maailma!";
+    @GetMapping("/kirjoittajat")
+    public String naytaKirjoittajat(Model model) {
+        model.addAttribute("vinkit", vinkkiRepository.findAll());
+        return "kirjoittajat";
+    }
+    
+    @PostMapping("/kirjoittajat")
+    public String lisaaVinkki(@RequestParam String kirjoittaja){
+        Vinkki uusiVinkki = new Vinkki();
+        uusiVinkki.setKirjoittaja(kirjoittaja);
+        vinkkiRepository.save(uusiVinkki);
+        return "redirect:/kirjoittajat";
     }
 }
