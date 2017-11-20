b8e74930030e290f2aab0f36ebff4c313f85ef6e
Jaana <jaana.embrich-hakala@helsinki.fi>
2017-11-20 17:25:08 +0200

    lisätty pari Junit testiä

--- src/test/java/ohtu/controller/VinkkiControllerTest.java
+++ src/test/java/ohtu/controller/VinkkiControllerTest.java
@@ -1,40 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ohtu.controller;
 
-
-
+import org.junit.Before;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
 
-public class VinkkiControllerTest {
+ @SpringBootTest
+ @AutoConfigureMockMvc
+ @RunWith(SpringJUnit4ClassRunner.class)
+ @WebAppConfiguration
+ 
+ public class VinkkiControllerTest {
+    
+ @Autowired
+ private MockMvc mockMvc;
 
-    @Autowired
-    private MockMvc mockMvc;
 
     @Test
     public void shouldReturnIndex() throws Exception {
         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
     }
     
+     
+    @Test
+	public void uriTemplate() throws Exception {
+            this.mockMvc.perform(post("/").param("kayttajanimi", "Jaana")
+            .param("salasana", "salasana"))
+                .andExpect(redirectedUrl("/vinkki"));
+	     
+        }
     
+  
 }
+
+
+
     
 
 
