e23a7de6c8c7e0ce5bb0242b9321c76618908b51
Dennis A <28817715+Dforssi@users.noreply.github.com>
2017-11-21 12:59:47 +0200

    Boolean validated ja kommenttiluonnostelun tarkennusta

--- src/main/java/ohtu/validator/Validator.java
+++ src/main/java/ohtu/validator/Validator.java
@@ -2,9 +2,10 @@ package ohtu.validator;
 
 public class Validator {
 
-    // private VinkkiI vinkkiI
+    // private VinkkiI vinkkiI;
+    private boolean validated;
 
-    //Public Validator(VinkkiI vinkki){konstruktori}
+    // Public Validator(VinkkiI vinkki){konstruktori}
 
     /**
      public void checkNull(){
@@ -13,16 +14,21 @@ public class Validator {
         VinkkiI.getTyyppi.notEmpty() && != null
      }
 
-
-     */
-
-
-    /**
      if(getTyyppi().equalsIgnoreCase("XXX"){
         check for class uniques
      }
 
-     */
+     *
+
+    */
+
+    public void setValidated(boolean validated){
+        this.validated = validated;
+    }
+
+    public boolean getValidated(){
+        return this.validated;
+    }
 
 
 
